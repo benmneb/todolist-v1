36388fdc29b953cc157909dd5a5cf3e89bb2322c
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-06-02 20:19:30 +0800

    add mongoDB and custom URLs

--- app.js
+++ app.js
@@ -1,48 +1,145 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js')
+const mongoose = require('mongoose');
+const capitalize = require('lodash.capitalize');
+
+const date = require(__dirname + '/date.js');
+const day = date.getDate();
 
 const app = express();
 
-let items = [];
-let workItems = [];
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const itemsSchema = {
+  name: {
+    type: String,
+    required: [true, 'Your to-do need a name!']
+  }
+}
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: 'Welcome to your To-Do List!'
+});
+const item2 = new Item({
+  name: 'Hit the + button to add a new item.'
+});
+const item3 = new Item({
+  name: '<-- Hit this to mark an item as done.'
+});
+const defaultItems = [item1, item2, item3];
+
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model('List', listSchema);
+
 
 app.listen('3000', () => {
   console.log('Listening to port 3000');
-})
+});
 
-app.get('/', (req, res) => {
-  const day = date.getDate();
-  res.render('list', {
-    listTitle: day,
-    newListItems: items
+app.get('/:customListName', (req, res) => {
+  const customListName = capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        // create new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect('/' + customListName);
+      } else {
+        // show existing list
+        res.render('list', {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
+      }
+    }
   });
 })
 
-app.post('/', (req, res) => {
-  let item = req.body.newItem;
-
-  if (req.body.button === 'Work') {
-    workItems.push(item);
-    res.redirect('/work');
-  } else {
-    items.push(item);
-    res.redirect('/');
-  }
-
-})
-
-app.get('/work', (req, res) => {
-  res.render('list', {
-    listTitle: 'Work List',
-    newListItems: workItems
+app.get('/', (req, res) => {
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully inserted default documents.');
+        }
+      });
+      res.redirect('/');
+    } else {
+      res.render('list', {
+        listTitle: 'To-Do-List',
+        newListItems: foundItems
+      });
+    }
   })
 })
+
+app.post('/', (req, res) => {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  if (listName === 'To-Do-List') {
+    item.save();
+    res.redirect('/');
+  } else {
+    List.findOne({name: listName}, (err, foundList) => {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect('/' + listName);
+    })
+  }
+})
+
+app.post('/delete', (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === 'To-Do-List') {
+    Item.findByIdAndRemove(checkedItemId, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Successfully deleted item.');
+        res.redirect('/');
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, (err, foundItem) => {
+      if (!err) {
+        res.redirect('/' + listName);
+      }
+    })
+  }
+
+});
 
 app.get('/about', (req, res) => {
   res.render('about')
